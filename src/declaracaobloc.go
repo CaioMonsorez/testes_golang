800c0e398eaac3ddbfa2e75f7b74579c6b7414f2
caioMonsorez <caio.monsoress@gmail.com>
2022-10-18 09:52:41 -0300

    testes com golang

---
+++ src/declaracaobloc.go
@@ -0,0 +1,26 @@
+package main
+import ("fmt")
+
+const (
+	p int = 8;
+)
+	
+
+func main() {
+   const (
+     a int = 3	
+     b int = 1
+     c string = "hello"
+   )
+
+	var d = a + b
+
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(p)
+
+	
+}
+
